905cebfc5105508b04e93446657a7bc37b059107
Rohit Mohan <rohit.mohan@gmail.com>
2018-10-16 14:16:34 -0400

    disallowed creating a collection without user_id

--- migrations/20181015203739-create-collection.js
+++ migrations/20181015203739-create-collection.js
@@ -4,8 +4,8 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('collections', { 
       id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
-      title: { type: Sequelize.STRING },
-      user_id: { type: Sequelize.INTEGER },
+      title: { type: Sequelize.STRING, allowNull: false },
+      user_id: { type: Sequelize.INTEGER, allowNull: false },
       created_at: { type: Sequelize.DATE, allowNull: false },
       updated_at: { type: Sequelize.DATE, allowNull: false }
     });
