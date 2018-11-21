0b6eca8cfbec33a12f3a5a3fdbcfb599583fa2b3
Rohit Mohan <rohit.mohan@gmail.com>
2018-11-20 19:10:40 -0500

    added temporary user lookup by email

--- app/models/item.js
+++ app/models/item.js
@@ -9,9 +9,6 @@ module.exports = (sequelize, DataTypes) => {
   const Item = sequelize.define('Item', itemSchema, {tableName: "items", underscored: true});
 
   Item.associate = function(models) {
-    Item.belongsToMany(models.User, {through: models.UserItem});
-    //Item.belongsToMany(models.Collection, { through: "collection_items", as: "collection", foreignKey: "collection_id" });
-    //Item.belongsToMany(models.Tag, { through: "item_tags", as: "tags", foreignKey: "tag_id" });
   };
 
   Item.exists = async url => {
--- app/models/user.js
+++ app/models/user.js
@@ -1,92 +1,56 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  const userSchema = {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    external_id: DataTypes.STRING, // id from an identity provider, like Google.
-    // the format of this field is <id-provider>-<id>, e.g. google-1234567
-    password: DataTypes.STRING,
-    salt: DataTypes.STRING
-  };
+    const userSchema = {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        external_id: DataTypes.STRING, // id from an identity provider, like Google.
+        // the format of this field is <id-provider>-<id>, e.g. google-1234567
+        password: DataTypes.STRING,
+        salt: DataTypes.STRING
+    };
 
-  const User = sequelize.define('User', userSchema, {tableName: "users", underscored: true});
+    const User = sequelize.define('User', userSchema, {tableName: "users", underscored: true});
 
-  User.associate = function(models) {
-    User.hasMany(models.Collection);
+    User.associate = function(models) {
+    };
 
-    //User.belongsToMany(models.UserType, { through: "user_types_users", as: "userTypes", foreignKey: "user_type_id" });
-    //User.belongsToMany(models.Subject, { through: "user_subjects", as: "subjects", foreignKey: "subject_id" });
-    User.belongsToMany(models.Item, {through: models.UserItem});
-  };
+    const isNumeric = value => !isNaN(value);
 
-  const isNumeric = value => !isNaN(value);
+    const isEmail = email => {
+        // regex for email taken from
+        //   https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  // class methods
-  User.findByExternalId = async function(externalId, options) {
-    var user = await sequelize.models.User.findOne(Object.assign({where: {external_id: externalId}}, options));
+        return re.test(email);
+    };
 
-    return user;
-  };
+    // class methods
+    User.findByEmail = async function(email, options) {
+        var user = await sequelize.models.User.findOne(Object.assign({where: {email: email}}, options));
 
-  User.find = async function(id, options) {
-    return isNumeric(id) ?
-        await sequelize.models.User.findById(id, options) :
-        await sequelize.models.User.findByExternalId(id, options);
-  };
-
-  // instance methods
-  User.prototype.addCollection = async function(title) {
-    var collection = await sequelize.models.Collection.create({title: title, user_id: this.id});
-
-    return collection;
-  };
-
-  User.prototype.associateItem = async function(item, comment, rating) {
-    await sequelize.models.UserItem.create({
-      user_id: this.id,
-      item_id: item.id,
-      rating: rating,
-      comment: comment
-    });
-  };
-
-  User.prototype.addItem = async function(title, url, options) {
-    // TODO: Check if an item with that URL already exists in the database.
-    // If so, check to see if the *user* already has that item. If *that's*
-    // true too, we're done. If not we need to manage the user and item
-    // association in the database
-    /*
-    var item = await sequelize.models.Item.exists(url);
-
-    if (item)
-    {
-      if (this.hasItem(item))
-      {
-        return item;
-      }
-    }
-    else
-    {
-      // create the item...
-      var item = await sequelize.models.Item.create({
-        title: title,
-        url: url
-      });
-    }
-    */
-
-    // create the item...
-    var newItem = await sequelize.models.Item.create({
-      title: title,
-      url: url
-    });
+        return user;
+    };
     
-    // ..., then associate the item with this user
-    await this.associateItem(newItem, options.comment || "", options.rating || 1);
+    User.findByExternalId = async function(externalId, options) {
+        var user = await sequelize.models.User.findOne(Object.assign({where: {external_id: externalId}}, options));
 
-    return newItem;
-  };
+        return user;
+    };
 
-  return User;
+    User.find = async function(id, options) {
+        if (isNumeric(id))
+        {
+            return await sequelize.models.User.findById(id, options);
+        }
+
+        if (isEmail(id))
+        {
+            return await sequelize.models.User.findByEmail(id, options);
+        }
+
+        return await sequelize.models.User.findByExternalId(id, options);
+    };
+
+    return User;
 };
